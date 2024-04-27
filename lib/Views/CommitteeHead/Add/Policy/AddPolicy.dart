e6a990085337fcfd8570076c236ac0abd1630f38
Abdul Islam Ghani <gmabdulislam@gmail.com>
2024-04-27 12:21:21 +0500

    Policy

--- lib/Views/CommitteeHead/Add/Policy/AddPolicy.dart
+++ lib/Views/CommitteeHead/Add/Policy/AddPolicy.dart
@@ -1,5 +1,6 @@
 
 
+import 'package:financial_aid/Components/CustomButton.dart';
 import 'package:financial_aid/Resources/CustomSize.dart';
 import 'package:flutter/material.dart';
 
@@ -11,13 +12,19 @@ class AddPolice extends StatefulWidget {
 }
 
 class _AddPoliceState extends State<AddPolice> {
+
+  String hintStr="Min cgpa required";
+  bool isTrue=false;
+
   List<String> policyFor=["NeedBase","MeritBase"];
 
   final TextEditingController _val1=TextEditingController();
   final TextEditingController _val2=TextEditingController();
+  final TextEditingController _amount=TextEditingController();
+  final TextEditingController _description=TextEditingController();
 
   String selectedVal="NeedBase";
-  String gVal="";
+  String gVal="CGPA";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,90 +33,141 @@ class _AddPoliceState extends State<AddPolice> {
         centerTitle: true,
         title:const Text("Add Police"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Policy For :",style: TextStyle(fontSize: CustomSize().customWidth(context)/20)),
-              SizedBox(
-                width: CustomSize().customWidth(context)/10,
-              ),
-              DropdownButton(
-                value: selectedVal,
-                  items:policyFor.map((e){
-                    return DropdownMenuItem(
-                        value: e.toString(),
-                        child: Text(e.toString()));
-                  }).toList(),
-                  onChanged: (val){
-                  selectedVal=val.toString();
-                  setState((){});
-              })
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                children: [
-                  Radio(value: "CGPA", groupValue: gVal, onChanged: (e){
-                    gVal=e.toString();
-                    setState(() {
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Policy For :",style: TextStyle(fontSize: CustomSize().customWidth(context)/20)),
+                SizedBox(
+                  width: CustomSize().customWidth(context)/10,
+                ),
+                DropdownButton(
+                  value: selectedVal,
+                    items:policyFor.map((e){
+                      return DropdownMenuItem(
+                          value: e.toString(),
+                          child: Text(e.toString()));
+                    }).toList(),
+                    onChanged: (val){
+                    selectedVal=val.toString();
+                    setState((){});
+                })
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Radio(value: "CGPA", groupValue: gVal, onChanged: (e){
+                      gVal=e.toString();
+                      hintStr="Min cgpa required";
+                      isTrue=false;
+                      setState(() {
 
-                    });
-                  }),
-                  const Text("CGPA")
-                ],
-              ),
-              SizedBox(
-                width: CustomSize().customWidth(context)/10,
-              ),
-              Row(
-                children: [
-                  Radio(value: "STRENGTH", groupValue: gVal, onChanged: (e){
-                    gVal=e.toString();
-                    setState(() {
+                      });
+                    }),
+                    const Text("CGPA")
+                  ],
+                ),
+                SizedBox(
+                  width: CustomSize().customWidth(context)/10,
+                ),
+                Row(
+                  children: [
+                    Radio(value: "STRENGTH", groupValue: gVal, onChanged: (e){
+                      gVal=e.toString();
+                      hintStr="Min Strength";
+                      isTrue=true;
+                      setState(() {
 
-                    });
-                  }),
-                  const Text("STRENGTH")
-                ],
-              ),
-            ],
-          ),
-          Padding(
-            padding: EdgeInsets.only(left:CustomSize().customWidth(context)/20,right: CustomSize().customWidth(context)/20,top: CustomSize().customWidth(context)/30),
-            child: TextFormField(
-              onChanged: (val){},
-              controller: _val1,
-              decoration: InputDecoration(
-                hintText: "minimum cgpa",
-                labelText: "minimum cgpa",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(CustomSize().customHeight(context)/60),
+                      });
+                    }),
+                    const Text("STRENGTH")
+                  ],
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(left:CustomSize().customWidth(context)/20,right: CustomSize().customWidth(context)/20,top: CustomSize().customWidth(context)/30),
+              child: TextFormField(
+                onChanged: (val){},
+                controller: _val1,
+                decoration: InputDecoration(
+                  hintText: hintStr,
+                  labelText: hintStr,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(CustomSize().customHeight(context)/60),
+                  ),
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left:CustomSize().customWidth(context)/20,right: CustomSize().customWidth(context)/20,top: CustomSize().customWidth(context)/30),
-            child: TextFormField(
-              onChanged: (val){},
-              controller: _val2,
-              decoration: InputDecoration(
-                hintText: "search",
-                labelText: "search",
-                suffixIcon:Icon(Icons.search,size: CustomSize().customWidth(context)/10),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(CustomSize().customHeight(context)/60),
+            Visibility(
+              visible: isTrue,
+              child: Padding(
+                padding: EdgeInsets.only(left:CustomSize().customWidth(context)/20,right: CustomSize().customWidth(context)/20,top: CustomSize().customWidth(context)/30),
+                child: TextFormField(
+                  controller: _val2,
+                  decoration: InputDecoration(
+                    hintText: "Max Strength",
+                    labelText: "Max Strength",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(CustomSize().customHeight(context)/60),
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+            Visibility(
+              visible: isTrue,
+              child: Padding(
+                padding: EdgeInsets.only(left:CustomSize().customWidth(context)/20,right: CustomSize().customWidth(context)/20,top: CustomSize().customWidth(context)/30),
+                child: TextFormField(
+                  onChanged: (val){},
+                  controller: _amount,
+                  decoration: InputDecoration(
+                    hintText: "Amount",
+                    labelText: "Amount",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(CustomSize().customHeight(context)/60),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left:CustomSize().customWidth(context)/20,right: CustomSize().customWidth(context)/20,top: CustomSize().customWidth(context)/30),
+              child: TextFormField(
+                onChanged: (val){},
+                controller: _description,
+                decoration: InputDecoration(
+                  hintText: "Description",
+                  labelText: "Description",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(CustomSize().customHeight(context)/60),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: CustomSize().customHeight(context)/10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomButton(title: "cancel", loading: false),
+                SizedBox(
+                  width: CustomSize().customWidth(context)/15,
+                ),
+                CustomButton(title: "Add", loading: false),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
