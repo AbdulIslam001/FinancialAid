cbabdaaab18cbcf274af8c9d61192ac50593ce51
Abdul Islam Ghani <gmabdulislam@gmail.com>
2024-05-31 17:01:43 +0500

    Info Container

--- lib/Views/CommitteeHead/CommitteeHeadDashBoard.dart
+++ lib/Views/CommitteeHead/CommitteeHeadDashBoard.dart
@@ -222,9 +222,31 @@ class _CommitteeHeadDashBoardState extends State<CommitteeHeadDashBoard> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             FutureBuilder(future: getSession(), builder: (context, snapshot) {
-              return Padding(
-                padding: EdgeInsets.only(left:CustomSize().customWidth(context)/100),
-                child: Center(child: Text(session,style: TextStyle(fontSize: CustomSize().customHeight(context)/40,fontStyle: FontStyle.italic),)),
+              return Row(
+                mainAxisAlignment:  MainAxisAlignment.spaceAround,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left:CustomSize().customWidth(context)/100),
+                    child: Container(
+                      height: CustomSize().customHeight(context)/25,
+                      width: CustomSize().customWidth(context)/3,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(CustomSize().customHeight(context)/80),
+                        color: Colors.blueGrey.withOpacity(0.2),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.white,
+                            spreadRadius: CustomSize().customHeight(context)/1000,
+                            blurRadius: CustomSize().customHeight(context)/100,
+                            offset: Offset(CustomSize().customHeight(context)/1400,
+                                CustomSize().customHeight(context)/1400),
+                          ),
+                        ],
+                      ),
+                      child:Center(child: Text(session,style: TextStyle(fontSize: CustomSize().customHeight(context)/70,fontStyle: FontStyle.italic),)),
+                    ),
+                  ),
+                ],
               );
             },),
             Row(
