053cd0aa103359ce205b04c9a04ee2036e6b4612
Abdul Islam Ghani <gmabdulislam@gmail.com>
2024-06-24 17:00:35 +0500

    commit

--- lib/Resources/AppUrl.dart
+++ lib/Resources/AppUrl.dart
@@ -1,7 +1,7 @@
 
 
 class EndPoint{
-  static String baseUrl="http://192.168.43.249/FinancialAidAllocation/";
+  static String baseUrl="http://192.168.1.42/FinancialAidAllocation/";
   static String login=baseUrl+"api/User/Login";
   static String getStudentInfo=baseUrl+"api/Student/getStudentInfo";
   static String checkApplicationStatus=baseUrl+"api/Student/getStudentApplicationStatus";
